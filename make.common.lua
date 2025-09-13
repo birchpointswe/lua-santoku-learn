d57e994b5323bf8a198b40b4abcc06a2bb85c08e
broma0 <121064585+broma0@users.noreply.github.com>
2025-09-13 18:53:40 -0400

    capi.c dataset.lua inv.h tokenizer.c: update _tm_graph_reconstruction +13
    lib/santoku/tsetlin/dataset.lua
      ~ M.anchor_pairs, M.random_pairs
    lib/santoku/tsetlin/evaluator/capi.c
      + tk_compute_stress, tm_optimize_bits_prefix_greedy
      - _tm_graph_reconstruction, tk_lua_verror, tm_optimize_bits_auc, tm_optimize_bits_generic, tm_optimize_bits_graph, tm_optimize_bits_prefix, tm_optimize_bits_prefix_only
    lib/santoku/tsetlin/inv.h
      + tk_inv_similarity_rank_filtered, tk_inv_weights_lua
    lib/santoku/tsetlin/tokenizer.c
      + tb_tokenizer_weights
    
    also: doc/l-sth.md, doc/todo.md, lib/santoku/tsetlin/graph.c, lib/santoku/tsetlin/graph.h, lib/santoku/tsetlin/inv.c, make.common.lua, test/spec/santoku/tsetlin/tokenizer.lua

--- make.common.lua
+++ make.common.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "santoku-tsetlin",
-  version = "0.0.136-1",
+  version = "0.0.137-1",
   variable_prefix = "TK_TSETLIN",
   license = "MIT",
   public = true,
@@ -35,16 +35,11 @@ local env = {
     "lua >= 5.1",
     "santoku >= 0.0.283-1",
     "santoku-threads >= 0.0.12-1",
-    "santoku-matrix >= 0.0.110-1",
+    "santoku-matrix >= 0.0.111-1",
     "santoku-system >= 0.0.56-1",
   },
 
   test = {
-
-
-
-
-
 
     dependencies = {
       "luacov >= 0.15.0-1",
