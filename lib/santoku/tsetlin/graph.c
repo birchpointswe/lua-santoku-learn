5f717fd2f650b2cb528324292be30143584f0eb7
broma0 <121064585+broma0@users.noreply.github.com>
2025-09-29 18:55:05 -0400

    mnist.lua spectral.c: update tk_spectral_matvec_normalized +3
    lib/santoku/tsetlin/spectral.c
      + tk_spectral_preconditioner
      - tk_spectral_matvec_normalized, tk_spectral_matvec_unnormalized
    test/spec/santoku/tsetlin/encoder/mnist.lua
      ~ each
    
    also: doc/hlth.md, doc/l-sth.md, lib/santoku/tsetlin/graph.c, lib/santoku/tsetlin/graph.h, test/spec/santoku/tsetlin/classify/mnist-corex.lua, test/spec/santoku/tsetlin/classify/mnist-simple.lua, test/spec/santoku/tsetlin/classify/mnist.lua

--- lib/santoku/tsetlin/graph.c
+++ lib/santoku/tsetlin/graph.c
@@ -151,15 +151,32 @@ static inline void tk_graph_worker (void *dp, int sig)
       for (uint64_t i = ifirst; i <= ilast; i ++) {
         int64_t u = uids[i];
         int64_t write = adj_offset[i];
-        int64_t iv, v;
-        double w;
-        tk_umap_foreach_keys(adj[i], iv, ({
-          v = uids[iv];
-          w = tk_graph_get_weight(graph, u, v);
+        tk_ivec_t *tmp_keys = tk_iuset_keys(0, adj[i]);
+        tk_ivec_asc(tmp_keys, 0, tmp_keys->n);
+        for (uint64_t j = 0; j < tmp_keys->n; j ++) {
+          int64_t iv = tmp_keys->a[j];
+          int64_t v = uids[iv];
+          double w = tk_graph_get_weight(graph, u, v);
           adj_data[write] = iv;
           adj_weights[write] = w;
           write ++;
-        }))
+        }
+        tk_ivec_destroy(tmp_keys);
+      }
+      break;
+    }
+
+    case TK_GRAPH_CSR_SOURCES: {
+      tk_ivec_t *adj_offset = data->adj_offset;
+      tk_ivec_t *adj_sources = data->adj_sources;
+      uint64_t ifirst = data->ifirst;
+      uint64_t ilast = data->ilast;
+      for (uint64_t i = ifirst; i <= ilast; i++) {
+        int64_t start = adj_offset->a[i];
+        int64_t end = adj_offset->a[i + 1];
+        for (int64_t j = start; j < end; j++) {
+          adj_sources->a[j] = (int64_t) i;
+        }
       }
       break;
     }
@@ -990,7 +1007,7 @@ static inline int tm_graph_adjacency (lua_State *L)
   if (graph->largest_component_root != -1) {
     uint64_t component_size = 0;
     tk_iumap_t *old_to_new = tk_iumap_create(0, 0);
-    tk_ivec_t *new_to_old = tk_ivec_create(L, 0, 0, 0);
+    tk_ivec_t *new_to_old = tk_ivec_create(0, 0, 0, 0);
 
     for (uint64_t old_idx = 0; old_idx < graph->uids->n; old_idx ++) {
       if (tk_dsu_findx(&graph->dsu, (int64_t)old_idx) == graph->largest_component_root) {
@@ -1037,7 +1054,18 @@ static inline int tm_graph_adjacency (lua_State *L)
 
     tk_iumap_destroy(old_to_new);
     tk_ivec_destroy(new_to_old);
-    return 4;
+
+    tk_ivec_t *adj_sources = tk_ivec_create(L, adj_data->n, 0, 0);
+    for (uint64_t i = 0; i < adj_offset->n - 1; i++) {
+      int64_t start = adj_offset->a[i];
+      int64_t end = adj_offset->a[i + 1];
+      for (int64_t j = start; j < end; j++) {
+        adj_sources->a[j] = (int64_t) i;
+      }
+    }
+    lua_insert(L, -4);
+
+    return 5;
   }
 
   uint64_t n_nodes = graph->uids->n;
@@ -1046,11 +1074,13 @@ static inline int tm_graph_adjacency (lua_State *L)
   tk_ivec_t *adj_offset = tk_ivec_create(L, n_nodes + 1, 0, 0);
   tk_ivec_t *adj_data = tk_ivec_create(L, 0, 0, 0);
   tk_dvec_t *adj_weights = tk_dvec_create(L, 0, 0, 0);
+  tk_ivec_t *adj_sources = NULL;
   for (unsigned int i = 0; i < graph->pool->n_threads; i ++) {
     tk_graph_thread_t *data = graph->threads + i;
     data->adj_offset = adj_offset;
     data->adj_data = adj_data;
     data->adj_weights = adj_weights;
+    data->adj_sources = NULL;
     tk_thread_range(i, graph->pool->n_threads, n_nodes, &data->ifirst, &data->ilast);
   }
 
@@ -1069,7 +1099,21 @@ static inline int tm_graph_adjacency (lua_State *L)
   tk_dvec_resize(adj_weights, (size_t) total, true);
   tk_threads_signal(graph->pool, TK_GRAPH_CSR_DATA, 0);
 
-  return 4;
+
+  adj_sources = tk_ivec_create(L, (size_t) total, 0, 0);
+  adj_sources->n = (size_t) total;
+  lua_insert(L, -4);
+
+
+  for (unsigned int i = 0; i < graph->pool->n_threads; i ++) {
+    tk_graph_thread_t *data = graph->threads + i;
+    data->adj_sources = adj_sources;
+  }
+
+
+  tk_threads_signal(graph->pool, TK_GRAPH_CSR_SOURCES, 0);
+
+  return 5;
 }
 
 static inline int tm_graph_pairs (lua_State *L)
