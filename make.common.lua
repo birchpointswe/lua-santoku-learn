a0ea4f891d2511562def69c4a7a64612d1172358
broma0 <121064585+broma0@users.noreply.github.com>
2026-08-10 19:36:36 -0400

    bump version to 2.0.0-1

--- make.common.lua
+++ make.common.lua
@@ -1,6 +1,6 @@
 local env = {
   name = "santoku-learn",
-  version = "1.0.0-1",
+  version = "2.0.0-1",
   variable_prefix = "TK_LEARN",
   license = "MIT",
   public = true,
@@ -37,15 +37,15 @@ local env = {
       },
     },
     dependencies = {
-      "santoku-system >= 1.0.0, < 2.0.0",
+      "santoku-system >= 2.0.0, < 3.0.0",
     },
   },
   dependencies = {
     "lua == 5.1",
-    "santoku >= 1.0.0, < 2.0.0",
-    "santoku-matrix >= 1.0.0, < 2.0.0",
-    "santoku-fs >= 1.0.0, < 2.0.0",
-    "santoku-lpeg >= 1.0.0, < 2.0.0",
+    "santoku >= 2.0.0, < 3.0.0",
+    "santoku-matrix >= 2.0.0, < 3.0.0",
+    "santoku-fs >= 2.0.0, < 3.0.0",
+    "santoku-lpeg >= 2.0.0, < 3.0.0",
     "lua-cjson >= 2.1.0.10-1",
   },
 }
